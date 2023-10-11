8943bc63f123d6c3c6f4a663cfef446226b92cf0
Woody Lin <wancheng.lin001@umb.edu>
2023-10-10 22:08:45 -0400

    Abstraction

---
+++ Abstraction/PasswordChangeEvent.java
@@ -0,0 +1,34 @@
+// import java.sql.Timestamp;
+
+// logic regarding any event lives in the abstract class, while logic regarding the 
+// specific event lives the appropriate class
+
+// public class PasswordChangeEvent implements Event {
+public class PasswordChangeEvent extends AbstractEvent{
+
+//     private final Long createdTimestamp;
+//     private final String id;
+
+    // it call AbstractEvent constructor
+    public PasswordChangeEvent(String id) {
+        super(id);
+    }
+//     
+//     public PasswordChangeEvent(String id) {
+//         this.id = id;
+//         this.createdTimestamp = new Timestamp(System
+//                 .currentTimeMillis()).getTime();
+//     }
+
+
+//     @Override
+//     public Long getTimeStamp() {
+//         return this.createdTimestamp;
+//     }
+    @Override
+    public void process() {
+        System.out.println("Customer " + id + " changed their password. " +
+                "Sending a confirmation email to the customer.");
+    }
+
+}
